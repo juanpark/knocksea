4656e58b0d65104cff88748f2d55cc6031f9e4d7
Happy-Lotus <happylotus145@gmail.com>
2025-07-01 13:53:28 +0900

    [Fix] Member DB default 설정 누락 수정

--- src/main/java/com/board/domain/Member.java
+++ src/main/java/com/board/domain/Member.java
@@ -1,5 +1,7 @@
 package com.board.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,12 +9,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
@@ -47,5 +51,6 @@ public class Member {
 
   @Enumerated(EnumType.STRING)
   @Column(nullable = false)
+  @ColumnDefault("'LOCAL'")
   private Platform platform;
 }
