ba78c7d44340d363d01135449d9be5775b5949cb
gusgo200 <gusgo200@naver.com>
2025-07-04 14:13:41 +0900

    [Feature] 인증된 게시글 작성자만 채택 가능 하도록 하면서 예외 처리 추가

--- src/main/java/com/board/exception/UnauthorizedCommentAdoptException.java
+++
@@ -1,7 +0,0 @@
-package com.board.exception;
-
-public class UnauthorizedCommentAdoptException extends SecurityException {
-    public UnauthorizedCommentAdoptException() {
-        super("댓글 채택 권한이 없습니다.");
-    }
-}
