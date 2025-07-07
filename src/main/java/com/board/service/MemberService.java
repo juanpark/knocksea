7d1ee0f70fed0a86be4eb13479f9cf80fbfee0f6
Happy-Lotus <happylotus145@gmail.com>
2025-07-07 10:04:09 +0900

    [Refactor] 회원가입 닉네임, 이메일 관련 예외 처리 수정

--- src/main/java/com/board/service/MemberService.java
+++ src/main/java/com/board/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.board.service;
 
 import com.board.domain.Member;
+import com.board.dto.MessageResponse;
 import com.board.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
   private final MemberRepository memberRepository;
+  private final MailService mailService;
 
   // 이름 및 닉네임 변경
   public Member updateMemberName(Member member) {
@@ -28,5 +30,26 @@ public class MemberService {
   // 사용자 닉네임 중복 확인
   public boolean checkNickname(String nickname){
     return memberRepository.existsByNickname(nickname);
+  }
+
+  //이메일 중복 확인 및 인증코드 전송
+  public MessageResponse sendVerification(String email){
+    if(this.findByEmail(email) != null){
+      throw new IllegalArgumentException("중복된 이메일입니다.");
+    }
+
+    return mailService.sendVerificationCode(email);
+  }
+
+  // 인증코드 검증
+  public MessageResponse verifyEmailCode(String email, String code){
+    boolean isValid = mailService.codeVerification(email, code);
+
+    if(!isValid){
+      throw new IllegalArgumentException("이메일 인증 실패");
+    }
+
+    return MessageResponse.builder().message("이메일 인증 성공").build();
+
   }
 }
